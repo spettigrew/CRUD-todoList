fb8532596943b6c0a21095b51ba6a89994844b62
spettigrew <sara.pettigrew5@gmail.com>
2019-09-16 20:10:16 -0600

    imported TaskForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import TaskForm from './tasks/taskForm/TaskForm';
 
 function App() {
     return (
         <div>
-            <h1>Hello!</h1>
+            <TaskForm />
         </div>
     )
 }
---
+++ src/components/tasks/taskForm/TaskForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function TaskForm() {
+    return (
+        <div>
+         <h1>Task Form</h1>   
+        </div>
+    )
+}
+
+export default TaskForm
