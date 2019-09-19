c86dec3667c414be0e6d9a9517d51cbc53dd7e00
spettigrew <sara.pettigrew5@gmail.com>
2019-09-19 13:19:31 -0600

    task form created

--- src/components/tasks/taskForm/TaskForm.js
+++ src/components/tasks/taskForm/TaskForm.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function TaskForm() {
+    const [formValues, setFormValues] = useState({
+        id: null, task: '', completed: false
+    })
+
+    function handleChange({target: {name, value}}) {
+        setFormValues({...formValues, [name]: value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formValues);
+    }
     return (
         <div>
-         <h1>Task Form</h1>   
+         <h1>Task Form</h1>
+         <form onSubmit={handleSubmit} >
+         <label>Task</label>
+         <input name='task' value={formValues.task} onChange={handleChange} />
+            </form>
         </div>
     )
 }
